0ade4efafacc02e3e95c55655c50eb4ce64a2a7a
156151516289 <53192279+elch01@users.noreply.github.com>
2022-06-06 19:31:16 +0200

    Update hash.sh

--- hash.sh
+++ hash.sh
@@ -2,11 +2,9 @@
 # Use wildcard otherwise you will have to specify a filename.
 echo "Enter path"
 read path
-FILES=$path
-
 for png in $path
 do
-md5sum -b $png | cut -d' ' -f1 | xxd -r -p | base64 >> hashes.txt
-awk '{print "/"$0"/"}' hashes.txt > hashes-4chanx.txt
-rm hashes.txt
+md5sum -b $png | cut -d' ' -f1 | xxd -r -p | base64 >> /tmp/hashes.tmp
+awk '{print "/"$0"/"}' /tmp/hashes.tmp > hashes.txt
+rm /tmp/hashes.tmp
 done
