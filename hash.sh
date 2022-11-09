b3a8642ab2b9ed92b9723aec93d6e3d8c9be1dd1
156151516289 <53192279+elch01@users.noreply.github.com>
2022-11-09 02:22:33 +0100

    Update hash.sh
    fix

--- hash.sh
+++ hash.sh
@@ -2,9 +2,7 @@
 # Use wildcard otherwise you will have to specify a filename.
 echo "Enter path"
 read path
-for png in $path
+for file in $path
 do
-md5sum -b $png | cut -d' ' -f1 | xxd -r -p | base64 >> /tmp/hashes.tmp
-awk '{print "/"$0"/"}' /tmp/hashes.tmp > hashes.txt
-rm /tmp/hashes.tmp
+md5sum -b $file | cut -d' ' -f1 | xxd -r -p | base64
 done
