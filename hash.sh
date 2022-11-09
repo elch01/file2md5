4aac007bc70b1fedde5bec8f2061785e4a57dbba
156151516289 <53192279+elch01@users.noreply.github.com>
2022-11-09 02:40:50 +0100

    Update hash.sh
    retard cant into foreach loops

--- hash.sh
+++ hash.sh
@@ -2,7 +2,10 @@
 # Use wildcard otherwise you will have to specify a filename.
 echo "Enter path"
 read path
+touch hashes.txt
 for file in $path
 do
 md5sum -b $file | cut -d' ' -f1 | xxd -r -p | base64
-done
+done > hashes.txt
+awk '{print "/"$0"/"}' hashes.txt > hashes-4chanx.txt
+rm hashes.txt
